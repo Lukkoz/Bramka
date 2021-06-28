16235f878cf9d17a41e05bcd8c8b46cf189f2a18
Łukasz Kozyra <ukikoza@gmail.com>
2021-06-28 21:21:55 +0200

    HTTP example stream

--- Skuterki/Skuterki.ino
+++ Skuterki/Skuterki.ino
@@ -16,3 +16,35 @@ void loop(){
 	Serial.println(current_gps_data.E,6);
 
 }
+
+/*
+AT+HTTPSET="URL","http://time.jsontest.com"  
+
+OK
+AT+HTTPACT=0  
+
+OK
+
++HTTP: 1
+
++HTTPRES: <0>,<200>,<342> 
+AT+HTTPREAD
+
+OK
+
++HTTPREAD: 342
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+Content-Type: application/json
+X-Cloud-Trace-Context: aa85a648a89dc60e93218b918552fffe
+Date: Mon, 28 Jun 2021 19:16:24 GMT
+Server: Google Frontend
+Content-Length: 100
+Connection: close
+
+{
+   "date": "06-28-2021",
+   "milliseconds_since_epoch": 1624907784983,
+   "time": "07:16:24 PM"
+}
+*/
